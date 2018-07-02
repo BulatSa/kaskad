4e193ada6cc2235374425303525279daac655328
Bulat <sadikovba@gmail.com>
2018-07-02 18:45:23 +0300

    Initial

---
+++ header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+	<title>Каскад</title>
+
+	<!-- favocons -->
+	<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+	<link rel="manifest" href="favicons/manifest.json">
+	<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="favicons/favicon.ico">
+	<meta name="msapplication-config" content="favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+	<!-- favocons -->
+
+	<!-- og -->
+	<meta property="og:title" content="Шаблон проекта">
+	<meta property="og:description" content="Описание проекта">
+	<meta property="og:type" content="website">
+	<meta property="og:image" content="/img/og.jpg">
+	<!-- og -->
+
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="mobile-menu">
+	<div class="mobile-menu__logo">
+		<img src="img/logo-mob-menu.png" alt="">
+	</div>
+	<nav class="mobile-menu__nav">
+		<ul>
+			<li>
+				<a href="#" class="scrollto">проекты</a>
+			</li>
+			<li>
+				<a href="#" class="scrollto">Услуги</a>
+			</li>
+			<li>
+				<a href="#" class="scrollto">цены</a>
+			</li>
+			<li class="red-promo">
+				<a href="#" class="scrollto">акции</a>
+			</li>
+			<li>
+				<a href="#" class="scrollto">о нас</a>
+			</li>
+			<li>
+				<a href="#" class="scrollto">отзывы</a>
+			</li>
+			<li>
+				<a href="#" class="scrollto">контакты</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="mobile-menu__callback">
+		<a href="#" class="btn btn--mini">Обратный звонок</a>
+	</div>
+</div>
+
+<section class="top-menu-sec">
+	<div class="top-menu container">
+		<div class="top-menu__burger">
+			<button class="hamburger hamburger--spin" type="button">
+			  <span class="hamburger-box">
+			    <span class="hamburger-inner"></span>
+			  </span>
+			</button>
+		</div>
+		<div class="top-menu__city">
+			<i class="i-geo"></i>
+			<span>Москва</span>
+		</div>
+		<nav class="top-menu__nav">
+			<ul>
+				<li>
+					<a href="#" class="scrollto">проекты</a>
+				</li>
+				<li>
+					<a href="#" class="scrollto">Услуги</a>
+				</li>
+				<li>
+					<a href="#" class="scrollto">цены</a>
+				</li>
+				<li class="red-promo">
+					<a href="#" class="scrollto">акции</a>
+				</li>
+				<li>
+					<a href="#" class="scrollto">о нас</a>
+				</li>
+				<li>
+					<a href="#" class="scrollto">отзывы</a>
+				</li>
+				<li>
+					<a href="#" class="scrollto">контакты</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="top-menu__phone">
+			<a href="tel:"><span>+7 (495)</span>602 92 92</a>
+		</div>
+	</div>
+</section>
---
+++ index.php
@@ -0,0 +1,40 @@
+﻿<?php include('header.php'); ?>
+
+<section class="big-head-sec">
+	<div class="big-head container">
+		<div class="big-head__top">
+			<div class="big-head__logo">
+				<img src="" alt="">
+				<span>Дизайн-проектирование<br>Отделочные работы</span>
+			</div>
+			<div class="big-head__callback">
+				<img src="" alt="">
+				<a href="#" class="btn">Обратный звонок </a>
+			</div>
+		</div>
+		<h1 class="big-head__title">Яркий дизайн вашей квартиры от 1000 рублей за м<sup>2</sup></h1>
+		<div class="big-head__benefits">
+			<div class="big-head__benefit">
+				<img src="" alt="">
+				<span><b>Поэтапная оплата</b>после вашего согласования</span>
+			</div>
+			<div class="big-head__benefit">
+				<img src="" alt="">
+				<span><b>Авторский надзор</b>руководителем дизайн-отдела</span>
+			</div>
+			<div class="big-head__benefit">
+				<img src="" alt="">
+				<span><b>Скидка 20%</b>при комплексном заказе</span>
+			</div>
+		</div>
+		<div class="big-head__contact">
+			<p class="big-head__contact-title">Заполните заявку на <span>бесплатную</span> консультацию дизайнера </p>
+			<div class="big-head__contact-btns">
+				<a href="#" class="btn">Заявка на консультацию</a>
+				<img src="img/big-head/prize.png" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
