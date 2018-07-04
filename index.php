476f2beed472104929f95ce9e68cdc3dac1995f1
Bulat <sadikovba@gmail.com>
2018-07-04 19:17:44 +0300

    Projects

--- index.php
+++ index.php
@@ -181,4 +181,138 @@
 	</div>
 </section>
 
+<section class="cases-sec def-sec-bottom">
+	<div class="cases-waves">
+		<img src="img/cases/bg-brushes.png" alt="">
+	</div>
+	<div class="cases container">
+		<div class="cases__top">
+			<div class="cases__info">
+				<div class="page-title">
+					<span>выполненные проекты</span>
+					<p class="h2">Эксклюзивный дизайн
+						и безупречный сервис</p>
+				</div>
+				<div class="user-content">
+					<p>Наша команда проектирует интерьеры в классическом и современном стилях для частных домов и коммерческих помещений. Многолетний опыт работы позволяет нам создавать красивые и уютные пространства, в которых приятно жить и работать.</p>
+				</div>
+			</div>
+			<img src="img/cases/valik.png" alt="">
+		</div>
+		<div class="cases__list row">
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="cases__item" style="background-image: url('../img/cases/room-1.jpg')">
+					<div class="cases__item-info">
+						<p class="h3">Лаконичный интерьер городской квартиры</p>
+						<a href="#" class="btn btn--flat fancy-modal" data-src="modal-room-1">Подробнее</a>
+						<span class="h3">60 м<sup>2</sup></span>
+					</div>
+					<div class="cases__item-slider"></div>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="cases__item" style="background-image: url('../img/cases/room-1.jpg')">
+					<div class="cases__item-info">
+						<p class="h3">Современная американская классика</p>
+						<a href="#" class="btn btn--flat fancy-modal" data-src="modal-room-1">Подробнее</a>
+						<span class="h3">90 м<sup>2</sup></span>
+					</div>
+					<div class="cases__item-slider"></div>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="cases__item" style="background-image: url('../img/cases/room-1.jpg')">
+					<div class="cases__item-info">
+						<p class="h3">Уголок Скандинавии в столице</p>
+						<a href="#" class="btn btn--flat fancy-modal" data-src="modal-room-1">Подробнее</a>
+						<span class="h3">160 м<sup>2</sup></span>
+					</div>
+					<div class="cases__item-slider"></div>
+				</div>
+			</div>
+
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="cases__item" style="background-image: url('../img/cases/room-1.jpg')">
+					<div class="cases__item-info">
+						<p class="h3">Лаконичный интерьер городской квартиры</p>
+						<a href="#" class="btn btn--flat fancy-modal" data-src="modal-room-1">Подробнее</a>
+						<span class="h3">60 м<sup>2</sup></span>
+					</div>
+					<div class="cases__item-slider"></div>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="cases__item" style="background-image: url('../img/cases/room-1.jpg')">
+					<div class="cases__item-info">
+						<p class="h3">Современная американская классика</p>
+						<a href="#" class="btn btn--flat fancy-modal" data-src="modal-room-1">Подробнее</a>
+						<span class="h3">90 м<sup>2</sup></span>
+					</div>
+					<div class="cases__item-slider"></div>
+				</div>
+			</div>
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="cases__item" style="background-image: url('../img/cases/room-1.jpg')">
+					<div class="cases__item-info">
+						<p class="h3">Уголок Скандинавии в столице</p>
+						<a href="#" class="btn btn--flat fancy-modal" data-src="modal-room-1">Подробнее</a>
+						<span class="h3">160 м<sup>2</sup></span>
+					</div>
+					<div class="cases__item-slider"></div>
+				</div>
+			</div>
+		</div>
+		<a href="#" class="cases__list-more">Посмотрите еще проекты</a>
+	</div>
+</section>
+
+<section class="akcia-sec def-sec">
+	<div class="akcia container">
+		<div class="page-title page-title--center">
+			<span>Акции</span>
+			<p class="h2">Сделайте выгодный ремонт с нами!</p>
+		</div>
+		<div class="akcia__list row">
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="akcia__item">
+					<img src="img/akcia/item-1.jpg" alt="">
+				</div>
+			</div>
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="akcia__item">
+					<img src="img/akcia/item-2.jpg" alt="">
+				</div>
+			</div>
+			<div class="grid-4 grid-6_s grid-12_xs">
+				<div class="akcia__item">
+					<img src="img/akcia/item-3.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="packages-sec def-sec">
+	<div class="packages container">
+		<div class="page-title page-title--center">
+			<span>пакеты дизайн-проектов</span>
+			<p class="h2">Семь раз отмерь, один раз закажи</p>
+		</div>
+		<div class="packages__input"></div>
+		<div class="packages__window">
+			<div class="packages__nav">
+				<a href="#" data-link="base">Базовый</a>
+				<a href="#" class="active" data-link="standart">Стандарт</a>
+				<a href="#" data-link="premium">Премиум</a>
+			</div>
+			<div class="packages__list">
+				<div class="packages__item" data-link="">
+
+				</div>
+			</div>
+		</div>
+
+	</div>
+</section>
+
 <?php include('footer.php'); ?>
